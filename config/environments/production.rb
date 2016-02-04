13ca00318d72057c7df97bf02a690cfb21086c96
kaltrine <kaltrine.kabashi@student.vinci.be>
2016-02-04 18:12:05 +0100

    environnement

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'sitepoint-devise.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'qwerteach.herokuapp.com' }
 
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
