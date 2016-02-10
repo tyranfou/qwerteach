3503239a79ed2cf34bfe7669988346a9a827e350
kaltrine <kaltrine.kabashi@student.vinci.be>
2016-02-10 11:19:11 +0100

    Ajout de CanCan pour la gestion des droits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
+
+
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
