432a6bb0f254f7b211482f32036eeb5872e9bf44
kaltrine <kaltrine.kabashi@student.vinci.be>
2016-03-07 11:19:44 +0100

    Verification année degree create update & maj level_id du user

--- app/controllers/degrees_controller.rb
+++ app/controllers/degrees_controller.rb
@@ -1,9 +1,10 @@
 class DegreesController < ApplicationController
-    load_and_authorize_resource
+  load_and_authorize_resource
 
   before_filter :find_degree, only: [:edit, :update, :destroy]
+
   def index
-    @degrees = Degree.where(:teacher=>current_user)
+    @degrees = Degree.where(:teacher => current_user)
   end
 
   def new
@@ -18,10 +19,15 @@ class DegreesController < ApplicationController
     @degree = Degree.new(degree_params)
     respond_to do |format|
       if @degree.save
+        teacher = @degree.teacher
+        if (teacher.level_id < @degree.level_id)
+          teacher.level_id=@degree.level_id
+          teacher.save
+        end
         format.html { redirect_to degrees_path, notice: "Degree successfully created" }
         format.json { render json: @degree, status: :created, location: @degree }
       else
-        format.html { redirect_to @degree, notice: 'Degree not created.'}
+        format.html { redirect_to @degree, notice: 'Degree not created.' }
         format.json { render json: @degree.errors, status: :unprocessable_entity }
       end
     end
@@ -32,6 +38,11 @@ class DegreesController < ApplicationController
 
   def update
     if @degree.update(degree_params)
+      teacher = @degree.teacher
+      if (teacher.level_id < @degree.level_id)
+        teacher.level_id=@degree.level_id
+        teacher.save
+      end
       redirect_to degrees_path, notice: "Degree successfully modified"
     else
       render 'edit', alert: 'Oops! il y a eu un problème...'
@@ -44,7 +55,7 @@ class DegreesController < ApplicationController
         format.html { redirect_to degrees_path, notice: 'Degree successfully deleted.' }
         format.json { render json: @degree, status: :created, location: @degree }
       else
-        format.html { redirect_to @degree, notice: 'Degree not deleted.'}
+        format.html { redirect_to @degree, notice: 'Degree not deleted.' }
         format.json { render json: @degree.errors, status: :unprocessable_entity }
       end
     end
