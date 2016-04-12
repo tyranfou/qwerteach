353cf7ff65a8df7cdf99a9c504ab78441ec4f159
kaltrine <kaltrine.kabashi@student.vinci.be>
2016-04-12 22:14:25 +0200

    Ask for lesson

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -6,6 +6,16 @@ class LessonsController < ApplicationController
     @lesson = Lesson.new
   end
 
+  def create
+    @student_id = current_user.id
+    @lesson = Lesson.new(lesson_params)
+    respond_to do |format|
+      if @lesson.save
+        format.html { redirect_to root_path, notice: 'Lesson was successfully required.' }
+      end
+    end
+  end
+
   def require_lesson
     @student_id = current_user.id
     @lesson = Lesson.new
@@ -13,6 +23,6 @@ class LessonsController < ApplicationController
 
   private
   def lesson_params
-    params.require(:lesson).permit(:student_id, :teacher_id, :price, :level_id, :topic_id, :topic_group_id, :time_start, :time_end)
+    params.require(:lesson).permit(:student_id, :teacher_id, :price, :level_id, :topic_id, :topic_group_id, :time_start, :time_end).merge(:student_id => current_user.id)
   end
 end
