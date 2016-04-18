f41ce2bb34fd50b988a2b32d61ce73f089b77747
Rouli <roulili.la.terreur@gmail.com>
2016-04-18 11:35:57 +0200

    display des notificatiosn en ajax, avec voir + fonctionnel

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -1,16 +1,35 @@
 $(document).ready(function(){
+  var limit = 3;  // limite initiale
+  var offset = 0; // offset initial
+  var shown = 0; // nombre de notificatiosn quie sont affichées ==> ré initialisé à "limit" si on ferme et rouvre le menu
+
   $('#notifications-dropdown').on('click', function (){
     if(!$(this).hasClass('open'))
     {
-      loadThreeNotifications();
+      shown = limit;
+      $('#notifications-wrapper').empty();
+      $('#notifications-wrapper').html('<i class="fa fa-spin fa-spinner"></i>');
+      loadNotifications(limit, offset);
     }
   });
-  var listeNotifs=[]
-  function loadThreeNotifications()
+
+  $('.see-more-notifications').on('click', function(e){
+    e.stopPropagation();
+    loadNotifications(limit, shown);
+    shown += limit;
+  });
+
+  function loadNotifications(limit, offset)
   {
-    $.get('/notifications', function(answer){
-        console.log(answer);
+    $.get('/notifications', {limit: limit, offset:offset} , function(answer){
+        $('#notifications-wrapper .fa-spin').remove();
         $('#notifications-wrapper').append(answer);
       }, 'html');
+  }
+
+
+  function numberOfUnread()
+  {
+
   }
 });
--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,11 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = current_user.mailbox.notifications.all
+    @notifications = current_user.mailbox.notifications.order('created_at DESC').limit(params[:limit]).offset(params[:offset])
     render :layout => false
+  end
+
+  def number_of_unread
+    render :json => current_user.mailbox.notifications.unread.count
   end
 end
