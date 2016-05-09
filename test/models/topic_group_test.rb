63a9751f7c87c74ec2929be7b2728ebc38b831e9
kkabash15 <kaltrine.kabashi@student.vinci.be>
2016-05-09 12:56:53 +0200

    fixtures for topics

--- test/models/topic_group_test.rb
+++ test/models/topic_group_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class TopicGroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+     assert_equal 6, TopicGroup.count
+  end
 end
