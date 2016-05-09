7171dd8caadc87b5e02466de599ba109ceedf41b
kkabash15 <kaltrine.kabashi@student.vinci.be>
2016-05-09 14:41:08 +0200

    une advert_price par advert et level

--- app/models/advert_price.rb
+++ app/models/advert_price.rb
@@ -7,5 +7,6 @@ class AdvertPrice < ActiveRecord::Base
   validates :level_id, presence: true
   validates :price, presence: true
   validates :price, :numericality => { :greater_than_or_equal_to => 0 }
+  validates_uniqueness_of :advert_id, scope: :level_id
 
 end
