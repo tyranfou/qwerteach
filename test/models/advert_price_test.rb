7a4eebf21442b01e0ba7d7c1db9f09405ca82ad7
David <luongd39@gmail.com>
2016-05-09 12:08:43 +0000

    Advert_Price / Users / Adverts - Fixture

--- test/models/advert_price_test.rb
+++ test/models/advert_price_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class AdvertPriceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "the truth" do
+     assert_equal 11, AdvertPrice.count
+   end
 end
