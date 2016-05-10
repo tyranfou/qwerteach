2ad6f9862d14ca01c3b2e315b981f69ca269c239
kkabash15 <kaltrine.kabashi@student.vinci.be>
2016-05-10 13:10:39 +0200

    Use normal fixtures + Gallery controller tests begun

--- spec/controllers/galleries_controller_spec.rb
+++ spec/controllers/galleries_controller_spec.rb
@@ -1,13 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe GalleriesController, type: :controller do
+  login_admin
+  describe GalleriesController do
+    before :each do
+      request.env['devise.mapping'] = Devise.mappings[:user]
+    end
+    it "should have a current_user" do
+      expect(subject.current_user).to_not eq(nil)
+    end
+    it "should get show" do
+      get 'show', :id => Gallery.first.id
+      expect(response).to be_success
+    end
+  end
 
-
-  it "should get index" do
-    # Note, rails 3.x scaffolding may add lines like get :index, {}, valid_session
-    # the valid_session overrides the devise login. Remove the valid_session from your specs
-    get 'index'
-    response.should be_success
+end
+RSpec.describe GalleriesController, type: :controller do
+  describe GalleriesController do
+    before :each do
+      request.env['devise.mapping'] = Devise.mappings[:user]
+    end
+    it "shouldn't have a current_user" do
+      expect(subject.current_user).to eq(nil)
+    end
+    it "shouldn't get show" do
+      get 'show', :id => Gallery.first.id
+      expect(response).to redirect_to new_user_session_path
+    end
   end
 
 end
