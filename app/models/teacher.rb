d6706a023f55f3bbf525152194918f95df29c861
Dewiid <soondavid@me.com>
2016-06-21 12:50:19 +0000

    Modification sur fonction ProfSimilaire - Teacher.rb

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -31,11 +31,35 @@ class Teacher  < Student
   end
 
   def similar_teachers(n)
-    a = Advert.where(user_id: id)
-    ids = a.map{|ad| ad.topic_id } #Get pour chaque Obj dans a les topics_id
-    adverts = Advert.where(topic_id: ids)
-    similar_teachers = adverts.map{|adv| adv.user}
 
-    similar_teachers.uniq{|u| u}.sample(n)
+    a = Advert.where(user_id: id) #Get Advert from User
+    ids = a.map{|ad| ad.topic_id }  #Get Topic from User
+    adverts = Advert.where(topic_id: ids) #Check advert where Topic = Topic
+    ids_user = adverts.map{|adv| adv.user_id} #Get User.id from advert
+    
+    idsProfSimi = []
+    ids_user.each do |id| 
+      if ids_user.include?(id) == true
+        idsProfSimi.push(id) #Si un prof apparait 2 fois alors Push dans le tableau 
+      end
+    end
+    
+    users = User.where(postulance_accepted: true) #Get prof postulance True
+    users_id = [] #Contiendra All teacher id
+    new_id = [] #Contiendra les users_id sélectionné
+    users_id = users.map{|u| u.id} #Get id des profs 
+    
+    if users_id.include?(id) #Check si le user n'est pas déjà dans le tab
+      users_id.delete(id) #Delete
+    end
+    
+    users_id.each do |u|
+      if idsProfSimi.include?(u) 
+        new_id.push(u) #Si un idTeacher apparait push dans un tab
+      end
+    end
+    new_ids = new_id.sample(n) #Sélection random de 4 teachers
+    @profSimis = new_ids.map{|id| User.find(id)} #Boum information user sélectionner 
+    
   end
 end
