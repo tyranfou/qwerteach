8dc2c9d9de3f0bda994df591182fec88f35dd93d
Rouli <roulili.la.terreur@gmail.com>
2016-07-15 16:52:29 +0200

    iframe classe de demo sidebar bonne couleur sidebar scroll

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -1,24 +1,5 @@
-function htmlbodyHeightUpdate(){
-    var height3 = $( window ).height()
-    var height1 = $('.nav').height()+50
-    height2 = $('.main-content').height();
-    // if(height2 > height3){
-    //     $('html').height(Math.max(height1,height3,height2)+10);
-    //     $('body').height(Math.max(height1,height3,height2)+10);
-    // }
-    // else
-    // {
-    //     $('html').height(Math.max(height1,height3,height2));
-    //     $('body').height(Math.max(height1,height3,height2));
-    // }
-}
 $(document).ready(function () {
-    htmlbodyHeightUpdate();
-    $( window ).resize(function() {
-        htmlbodyHeightUpdate()
-    });
     $( window ).scroll(function() {
-        htmlbodyHeightUpdate();
         footerScroll();
     });
 });
@@ -27,25 +8,40 @@ function footerScroll(){
     h1 = $('#footer').offset().top - $(body).height();
     if($(window).scrollTop() > h1)
     {
+        // Footer visible
         $('nav.sidebar').css({
             position: 'absolute',
-            top: h1+50
+            top: h1
         });
         $('#sidebar-profile').css({
             position: 'absolute',
-            top: h1+50,
+            top: h1,
             width:256
         });
     }
-    else 
+    else if($(window).scrollTop() >= 50)
     {
+        // footer et navbar invisibles
         $('nav.sidebar').css({
             position: 'fixed',
-            top: 50
+            top: 0
         });
         $('#sidebar-profile').css({
             position: 'fixed',
-            top: 50,
+            top: 0,
+            width: '20%'
+        });
+    }
+    else
+    {
+        //navbar visible
+        $('nav.sidebar').css({
+            position: 'relative',
+            top: 0
+        });
+        $('#sidebar-profile').css({
+            position: 'relative',
+            top: 0,
             width: '20%'
         });
     }
