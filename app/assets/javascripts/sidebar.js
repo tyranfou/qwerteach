965a1f588522e103acd8dc21b7a970041d60d6db
Rouli <roulili.la.terreur@gmail.com>
2016-07-21 15:17:54 +0200

    fix sidebar sur petites pages

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -7,17 +7,20 @@ $(document).ready(function () {
 });
 
 function bodySidebarHeight(){
-    h1 = $('.main-content').height();
+    h1 = $('.main-content').innerHeight();
     h2 = $('.sidebar').height();
-    if(h2<h1){
-        $('.main-content').height($('.sidebar').height());
+    h3 = $(body).height();
+    if(h1<h3){
+        console.log('resize');
+        $('.main-content').height(h3);
     }
 }
 
 function footerScroll(){
     h1 = $('#footer').offset().top - $(body).height();
-    if($(window).scrollTop() > h1)
+    if($(window).scrollTop() > h1 )
     {
+        console.log('footer');
         // Footer visible
         $('nav.sidebar').css({
             position: 'absolute',
@@ -29,21 +32,9 @@ function footerScroll(){
             width:256
         });
     }
-    else if($(window).scrollTop() >= 50)
+    else  if($(window).scrollTop() <= 50)
     {
-        // footer et navbar invisibles
-        $('nav.sidebar').css({
-            position: 'fixed',
-            top: 0
-        });
-        $('#sidebar-profile').css({
-            position: 'fixed',
-            top: 0,
-            width: 256
-        });
-    }
-    else
-    {
+        console.log('nav');
         //navbar visible
         $('nav.sidebar').css({
             position: 'relative',
@@ -52,6 +43,20 @@ function footerScroll(){
         $('#sidebar-profile').css({
             float: 'left',
             position: 'relative',
+            top: 0,
+            width: 256
+        });
+    }
+else
+    {
+        console.log('none');
+        // footer et navbar invisibles
+        $('nav.sidebar').css({
+            position: 'fixed',
+            top: 0
+        });
+        $('#sidebar-profile').css({
+            position: 'fixed',
             top: 0,
             width: 256
         });
