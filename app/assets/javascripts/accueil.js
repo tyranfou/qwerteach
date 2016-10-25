ea0d0087726eabd134af129d315e430bae03a667
basilebong <info@basilebong.com>
2016-10-25 16:28:46 +0000

    Acceuil : Bar de recherche

--- app/assets/javascripts/accueil.js
+++ app/assets/javascripts/accueil.js
@@ -1,11 +1,25 @@
 $(window).ready(function(e){
     
+    var width = $(window).width();
+    
     $(".prof").each(function(i){
         $(this).addClass("item" + i)
     })
+    
+    //toggle CCM 
     
     $(".toggleCCM").click(function(){
         $(".section_commentmarche").toggle("slow");
     })
     
+    //largeur de la abr de recherche 
+    $(".recherche-bar").css("width", width - 50);
+    window.addEventListener("resize", function(){
+        width = $(window).width();
+        $(".recherche-bar").css("width", width -50);
+    });
+    window.addEventListener("orientationchange", function(){
+        width = $(window).width();
+        $(".recherche-bar").css("width", width -50);
+    });
 })
