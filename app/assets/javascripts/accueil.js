5ba6892638f56fa7413f9f10c7411b53fe388a6d
Rouli <roulili.la.terreur@gmail.com>
2016-11-23 14:49:22 +0100

    section profs resize in js

--- app/assets/javascripts/accueil.js
+++ app/assets/javascripts/accueil.js
@@ -22,4 +22,23 @@ $(window).ready(function(e){
         width = $(window).width();
         $(".recherche-bar").css("width", width -300);
     });
+    resizeTeachers();
+
+    function resizeTeachers(){
+        var u = $(body).width() / 12;
+        $('.section_profs .title').css({top:0, width:u*4, height: u*4});
+        $('.section_profs #prof0').css({top:0, left:u*4, width: u*5, height: u*5});
+        $('.section_profs #prof1').css({top:0, left:u*9, width: u*3, height:u*3});
+        $('.section_profs #prof2').css({top:u*4, left:-u, width: u*2, height: u*2});
+        $('.section_profs #prof3').css({top:u*4, left:u, width: u*2, height: u*2});
+        $('.section_profs #prof4').css({top:u*4, left:u*3, width: u, height: u});
+        $('.section_profs #prof5').css({top:u*3, left:u*9, width: u*3/2, height: u*3/2});
+        $('.section_profs #prof6').css({top:u*3+u*3/2, right:u*3/2, width: u*3/2, height: u*3/2});
+        $('.section_profs #prof7').css({top: u*3, right:-u*3/2, width: u*3, height:u*3});
+        $('.section_profs #prof8').css({top:6*u, left:0, width: u*3, height: u*3});
+        $('.section_profs #prof9').css({top:5*u, left:3*u, width: u*3, height: u*3});
+        $('.section_profs #prof10').css({top:5*u, left:6*u, width: u*3, height: u*3});
+        $('.section_profs #prof11').css({top:6*u, left:9*u, width: u*3, height: u*3});
+        $('.section_profs').css({maxHeight: u*9});
+    }
 })
