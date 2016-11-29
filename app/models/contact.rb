a9baa4c201f7f834c499ec5fad024572884858f4
root <root@localhost.localdomain>
2016-11-29 15:41:58 +0100

    adding contact form

---
+++ app/models/contact.rb
@@ -0,0 +1,19 @@
+
+class Contact < MailForm::Base
+  attribute :name,      :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :subject,      :validate => true
+  attribute :message
+
+  attribute :nickname,  :captcha  => true
+
+  # Declare the e-mail headers. It accepts anything the mail method
+  # in ActionMailer accepts.
+  def headers
+    {
+      :subject => subject,
+      :to => "hello@qwerteach.com",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
+end
